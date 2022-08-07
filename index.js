b4eedd1c2a00f8494140db8f8f9b3e790aa7a2d5
web-dev-champ <110686082+web-dev-champ@users.noreply.github.com>
2022-08-07 13:19:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ let movies = [
 		name: "falcon and the winter soldier",
 		des:
 			"Following the events of Avengers: Endgame, Sam Wilson and Bucky Barnes team up in a global adventure that tests their abilities and their patience.",
-		image: "images/slider 2.PNG"
+		image: "images/slider 2.png"
 	},
 	{
 		name: "loki",
 		des:
 			"The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of Avengers Endgame.",
-		image: "images/slider 1.PNG"
+		image: "images/slider 1.png"
 	},
 	{
 		name: "wanda vision",
 		des:
 			"Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.",
-		image: "images/slider 3.PNG"
+		image: "images/slider 3.png"
 	},
 	{
 		name: "raya and the last dragon",
 		des:
 			"Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.",
-		image: "images/slider 4.PNG"
+		image: "images/slider 4.png"
 	},
 	{
 		name: "luca",
 		des:
 			"Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. ",
-		image: "images/slider 5.PNG"
+		image: "images/slider 5.png"
 	}
 ];
 
